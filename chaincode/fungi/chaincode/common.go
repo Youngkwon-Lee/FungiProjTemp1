d7b4e730e0485f1108a3a57bb759af70833d2c39
Youngkwon Lee <kwon3856@naver.com>
2023-09-11 18:49:22 +0000

    complete createRandomFungus funcction

--- chaincode/fungi/chaincode/common.go
+++ chaincode/fungi/chaincode/common.go
@@ -6,7 +6,7 @@ import (
 )
 
 // AssetExists returns true when asset with given ID exists in world state
-func (s *SmartContract) _AssetExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
+func (s *SmartContract) _assetExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
 	assetJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
 		return false, fmt.Errorf("failed to read from world state: %v", err)
@@ -15,8 +15,32 @@ func (s *SmartContract) _AssetExists(ctx contractapi.TransactionContextInterface
 	return assetJSON != nil, nil
 }
 
+func (s *SmartContract) _getstate(ctx contractapi.TransactionContextInterface, id string) ([]byte, error) {
+	assetJSON, err := ctx.GetStub().GetState(id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from world state: %v", err)
+	}
+	if assetJSON == nil {
+		return nil, fmt.Errorf("the asset %s does not exist", id)
+	}
+	return assetJSON. nill
 
-// 외부에서 호출 가능한 interface....
-// 초기화에 관련된 함수
-// 버섯생성 (CreateRandomFungus )
-// 버섯조회 (GentFungiByOwner )
+}
+
+func (s *SmartContract) _updateOwnerFungusCount(ctx contractapi.TransactionContextInterface, clientID string, increment int ) error {
+	countBtye, err := s.GetState(ctx, clientId)
+	if countBtye == nil {
+		ctx.GetStub().Putstate(clientId, []byte(strconv.IToa(1)))
+		return nil
+	}
+	if err !=nil {
+		return err
+	}
+	ownerfunguscount, _ := strconv.Atoi(countBtye[:])
+	ownerfunguscount += increment
+	err = ctx.GetStub().Putstate(clientId, []byte(strconv.IToa(ownerfunguscount)))
+	if err != nil {
+		return fmt.Errorf("failed to put /ownerFungusCount state: %v", err)
+	}
+	return nil
+}
--- chaincode/fungi/chaincode/smartcontract.go
+++ chaincode/fungi/chaincode/smartcontract.go
@@ -6,6 +6,7 @@ import (
 	"time"
 	"crypto/sha256"
 	"encoding/binary"
+	"encoding/json"
 	"math"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
@@ -60,29 +61,83 @@ func (s *SmartContract) Initialized (ctx contractapi.TransactionContextInterface
 
 func (s *SmartContract) CreateRandomFungus (ctx contractapi.TransactionContextInterface, name string) error {
 
-		//Check aclientID
+	//Check aclientID
 	clientId, err := ctx.GetClientIdentity().GetID()
 	if err != nil {
-		return fmt.Errorf("failed to get ClientID : %v ", err)
+		return fmt.Errorf("failed to get ClientID : %v ", err) 		
 	}
-	exists, err := s._assetExists(ctx. clientId)
-	if err !=nil{
+
+	exists, err := s._assetExists(ctx, clientID)
+	if err != nil {
 		return err
 	}
 	if exists {
-		return fmt.Errorf("client has alreadyc created an initial fungus")
+		return fmt.Errorf("client has already created an intial fungus")
 	}
-	 // 랜덤 DNA 생성
-	 // 버섯 생성 및 원장에 저장하는 함수
-	 //clientID 를 key 값으로 하는 값을 +1 저장 (보유한 버섯의 수)
+
+	// 랜덤 DNA 생성
+	dna := s._generateRandomDna(ctx,name)
+	//버섯 생성 및 원장에 저장하는 함수
+
+	s._createfungus(ctx,name,dna)
 	return nil
 }
 
+func (s *SmartContract) _createfungus (ctx contractapi.TransactionContextInterface, name string, dna uint) error {
 
-func (s *SmartContract) _generateRandomDna (ctx contractapi.TransactionContextInterface, name string) uint {
+	fungusCountBytes, err := s._getstate(ctx, fungusCountKey)
+	if err != nil {
+		return err
+	}
+	fungusIdINT, _ := strconv.Atoi(string(fungusCountBytes))
+	fungusId := uint(fungusIdINT)	
 
+	//Check aclientID
+	clientId, err := ctx.GetClientIdentity().GetID()
+	if err != nil {
+		return fmt.Errorf("failed to get ClientID : %v ", err) 		
+	}
+
+	nowTime := time.Now().Unix()									
+	readyTime := nowTime + 60
+		
+	fungus := Funguus{
+		fungusId: fungusId,
+		Name:	  name,
+		Owner:    clientId,
+		Dna:      dna,
+		ReadyTime: uint32(readyTime),
+	}
+
+	// marshal fungusID
+	assetJSON, err := json.Marshal(fungus)
+	if err != nil (
+		return fmt.Errorf("failed to marshal fungus : %v ", err)
+	)
+    // Putstate fungusID
+	err = ctx.GetStub().PutState(strconv Itoa(int(fungusid)), assetJSON)
+	if err != nil (
+		return fmt.Errorf("failed to put fungus : %v ", err)
+	)
 	
+	// fungusCount ++
+	FungusId += 1
+	err = ctx.GetStub().PutState(fungusCountKey, []byte(strconv.Itoa(int(fungusId))))
+	if err != nil (
+		return fmt.Errorf("failed to put fungus : %v ", err)
+	)
+	//ownerfunguscount ++
+
+	err = s._updateOwnerFungusCount(ctx,clientId, 1)
+	if err != nil (
+		return err
+	)
+	return nil
+}
 	
+
+	func (s *SmartContract) _generateRandomDna (ctx contractapi.TransactionContextInterface, name string) uint {
+
 	unixTime := time.Now().Unix()
 	data := strconv.Itoa(int(unixTime)) + name
 	hash := sha256.New()
